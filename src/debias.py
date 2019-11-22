9c7abbc003c3bf6cb32ecd465535497bfd83d0a6
Shauli Ravfogel <shauli321@gmail.com>
2019-11-22 13:11:59 +0200

    random projections, noise, several classifiers

--- src/debias.py
+++ src/debias.py
@@ -3,6 +3,11 @@ import scipy
 from scipy import linalg
 from typing import Tuple
 import classifier
+import svm_classifier
+from sklearn.svm import LinearSVC, SVC
+from sklearn.neural_network import MLPClassifier
+from sklearn.linear_model import SGDClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
 def get_nullspace_projection(W: np.ndarray) -> np.ndarray:
     """
@@ -18,7 +23,7 @@ def get_nullspace_projection(W: np.ndarray) -> np.ndarray:
 
 
 def get_debiasing_projection(classifier_class: classifier.Classifier, num_classifiers: int, input_dim: int, is_autoregressive: bool,
-           min_accuracy: float, X_train: np.ndarray, Y_train: np.ndarray, X_dev: np.ndarray, Y_dev: np.ndarray) -> np.ndarray:
+           min_accuracy: float, X_train: np.ndarray, Y_train: np.ndarray, X_dev: np.ndarray, Y_dev: np.ndarray, noise = False, random_projection = False) -> np.ndarray:
 
     """
 
@@ -37,16 +42,31 @@ def get_debiasing_projection(classifier_class: classifier.Classifier, num_classi
     P = np.eye(input_dim)
     X_train_cp = X_train.copy()
     X_dev_cp = X_dev.copy()
+    
+    if noise:
+        print("Adding noise.")
+        mean = np.mean(np.abs(X_train))
+        mask_train = 0.01 * (np.random.rand(*X_train.shape) - 0.5)
+        
+        X_train_cp += mask_train
 
     for i in range(num_classifiers):
 
-        clf = classifier_class()
+        #clf = classifier_class()
+        if np.random.random() < 0.2:
+                clf = svm_classifier.SVMClassifier(SGDClassifier(max_iter=7500, loss = "perceptron", penalty = "l2"))
+                #clf = svm_classifier.SVMClassifier(LinearDiscriminantAnalysis(n_components = 1))
+        else:
+                clf = svm_classifier.SVMClassifier(LinearSVC(max_iter=25000, fit_intercept=False, class_weight="balanced", penalty="l2", dual=False))
+                
         acc = clf.train(X_train_cp, Y_train, X_dev_cp, Y_dev)
+        print("Iteration {}, Accuracy: {}".format(i, acc))
         if acc < min_accuracy: continue
 
         W = clf.get_weights()
         P_i = get_nullspace_projection(W)
-        P = P_i.dot(P)
+        #P = P_i.dot(P)
+        P = P.dot(P_i)
 
         if is_autoregressive:
 
