fixing numerical errors: enforcing orthogonality
Errors stem from (1) inexact calculation of nullspace projection (2) w_i != argmin[L], making it not exactly orthogonal to the previous ones
now enforcing orthogonality by calculating explicit projection to the intersection of the nullspaces at each iteration.

src/debias.py
@@ -2,8 +2,13 @@ from typing import Dict
 
 import numpy as np
 import scipy
-from src import svm_classifier
-
+import svm_classifier
+#import svm_classifier
+from sklearn.svm import LinearSVC, SVC
+from sklearn.linear_model import Perceptron, SGDClassifier
+import matplotlib
+import matplotlib.pyplot as plt
+REGRESSION = False
 from typing import List
 from tqdm import tqdm
 import random
@@ -15,10 +20,26 @@ def get_nullspace_projection(W: np.ndarray) -> np.ndarray:
     :return: the projection matrix
     """
     nullspace_basis = scipy.linalg.null_space(W)  # orthogonal basis
+    print(nullspace_basis)
+    exit()
     nullspace_basis = nullspace_basis * np.sign(nullspace_basis[0][0])  # handle sign ambiguity
     projection_matrix = nullspace_basis.dot(nullspace_basis.T)
 
     return projection_matrix
+    
+    
+def get_rowspace_projection(W: np.ndarray) -> np.ndarray:
+    """
+    :param W: the matrix over its nullspace to project
+    :return: the projection matrix
+    """
+
+    w_basis = scipy.linalg.orth(W.T) # orthogonal basis
+    w_basis * np.sign(w_basis[0][0]) # handle sign ambiguity
+    P_W = w_basis.dot(w_basis.T) # orthogonal projection on W's rowspace
+    
+    return P_W
+
 
 
 def debias_by_specific_directions(directions: List[np.ndarray], input_dim: int):
@@ -33,8 +54,8 @@ def debias_by_specific_directions(directions: List[np.ndarray], input_dim: int):
 def get_debiasing_projection(classifier_class, cls_params: Dict, num_classifiers: int, input_dim: int,
                              is_autoregressive: bool,
                              min_accuracy: float, X_train: np.ndarray, Y_train: np.ndarray, X_dev: np.ndarray,
-                             Y_dev: np.ndarray, noise=False, by_class=True, Y_train_main=None,
-                             Y_dev_main=None) -> np.ndarray:
+                             Y_dev: np.ndarray, by_class=True, Y_train_main=None,
+                             Y_dev_main=None, by_rowspace = True) -> np.ndarray:
     """
     :param classifier_class: the sklearn classifier class (SVM/Perceptron etc.)
     :param cls_params: a dictionary, containing the params for the sklearn classifier
@@ -46,31 +67,28 @@ def get_debiasing_projection(classifier_class, cls_params: Dict, num_classifiers
     :param Y_train: ndarray, training labels (protected attributes)
     :param X_dev: ndarray, eval vectors
     :param Y_dev: ndarray, eval labels (protected attributes)
-    :param noise: bool, whether to add noise to the vectors
     :param by_class: if true, at each iteration sample one main-task label, and extract the protected attribute only from vectors from this class
     :param T_train_main: ndarray, main-task train labels
     :param Y_dev_main: ndarray, main-task eval labels
-    :return: the debiasing projection
+    :param by_rospace: bool. If true, calculate P_N(wi) by the relation P_N(wi) = I - P_(wi)
+    :return: P, the debiasing projection; rowspace_projections, the list of all rowspace projection; Ws, the list of all calssifiers.
     """
-
+    
+    I = np.eye(input_dim)
+    
     if by_class:
         if ((Y_train_main is None) or (Y_dev_main is None)):
             raise Exception("Need main-task labels for by-class training.")
         main_task_labels = list(set(Y_train_main.tolist()))
                             
-    P = np.eye(input_dim)
     X_train_cp = X_train.copy()
     X_dev_cp = X_dev.copy()
-                            
-    if noise:
-        print("Adding noise.")
-        mean = np.mean(np.abs(X_train))
-        mask_train = 0.0075 * (np.random.rand(*X_train.shape) - 0.5)
-        X_train_cp += mask_train
-
+    rowspace_projections = []
+    Ws = []
+    
     pbar = tqdm(range(num_classifiers))
     for i in pbar:
-                            
+        
         clf = svm_classifier.SVMClassifier(classifier_class(**cls_params))
 
         if by_class:
@@ -81,40 +99,52 @@ def get_debiasing_projection(classifier_class, cls_params: Dict, num_classifiers
             relevant_idx_train = np.ones(X_train_cp.shape[0], dtype=bool)
             relevant_idx_dev = np.ones(X_dev_cp.shape[0], dtype=bool)
 
-        acc = clf.train_network(X_train_cp[relevant_idx_train], Y_train[relevant_idx_train], X_dev_cp[relevant_idx_dev],
-                                Y_dev[relevant_idx_dev])
+        acc = clf.train_network(X_train_cp[relevant_idx_train], Y_train[relevant_idx_train], X_dev_cp[relevant_idx_dev], Y_dev[relevant_idx_dev])
         pbar.set_description("iteration: {}, accuracy: {}".format(i, acc))
         if acc < min_accuracy: continue
 
         W = clf.get_weights()
-        P_i = get_nullspace_projection(W)
-        P = P.dot(P_i)
+        Ws.append(W)
+        P_rowspace_wi = get_rowspace_projection(W) # projection to W's rowspace
+        rowspace_projections.append(P_rowspace_wi)
 
         if is_autoregressive:
-            X_train_cp = X_train_cp.dot(P_i)
-            X_dev_cp = X_dev_cp.dot(P_i)
+            
+            # to ensure numerical stability, explicitly project to the intersection of the nullspaces found so far (instaed of doing X = P_iX, which is problematic when w_i is not exactly orthogonal to w_i-1,...,w1, due to e..g problems in finding the minimum
+            
+            # use the intersection-projection formula of Ben-Israel 2013 http://benisrael.net/BEN-ISRAEL-NOV-30-13.pdf: N(w1)∩ N(w2) ∩ ... ∩ N(wn) = N(P_R(w1) + P_R(w2) + ... + P_R(wn))
+            Q = np.sum(rowspace_projections, axis = 0)
+            P = I - get_rowspace_projection(Q)
+                 
+            # project            
+            X_train_cp = (P.dot(X_train.T)).T
+            X_dev_cp = (P.dot(X_dev.T)).T
 
-    return P
+    # calculae final projection matrix P=P1P2..PN 
+    # since w_i.dot(w_i-1) = 0, P1P2 = P1 - P2 - I (proof in the paper); this is more numerically stable.
+    # by induction, P1P2..PN = I - (P1+..+PN). We will use instead Ben-Israel's formula to increase stability
+    
+    Q = np.sum(rowspace_projections, axis = 0)
+    P = I - get_rowspace_projection(Q)
+    P2 = I - np.sum(rowspace_projections, axis = 0)
+    print("test: {}".format(P2.dot(P2) - P2))
+    return P, rowspace_projections, Ws
 
 
 if __name__ == '__main__':
-    X = np.random.rand(5000, 300)
-    Y = np.random.rand(5000) < 0.5
-    num_classifiers = 15
-    classifier_class = None
-    input_dim = 300
+    N = 10000
+    d = 5
+    X = np.random.rand(N, d) - 0.5
+    Y = np.array([1 if sum(x) > 0 else 0 for x in X]) #X < 0 #np.random.rand(N) < 0.5 #(X + 0.01 * (np.random.rand(*X.shape) - 0.5)) < 0 #np.random.rand(5000) < 0.5
+    #Y = np.array(Y, dtype = int)
+    
+    num_classifiers = 3
+    classifier_class = SGDClassifier #Perceptron
+    input_dim = d
     is_autoregressive = True
-    min_accuracy = 0.5
+    min_accuracy = 0.0
     noise = False
     random_subset = True
-    siamese = True
-
-    P = get_debiasing_projection(classifier_class, None, num_classifiers, input_dim, is_autoregressive, min_accuracy,
-                                 X, Y, X, Y, by_class=True)
-
-    print(list(zip(X.dot(P)[0], X.dot(P).dot(P)[0]))[:10])
-
-    print(list(zip(P[0], P.dot(P)[0]))[:10])
-    # assert np.allclose(P.dot(P), P)
-    # assert np.allclose(P.dot(P), P.T)
-    print("yay")
+    
+    P = get_debiasing_projection(classifier_class, {"max_iter": 15}, num_classifiers, input_dim, is_autoregressive, min_accuracy, X, Y, None, None, by_class = False)
+    