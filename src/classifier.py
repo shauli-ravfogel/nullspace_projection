1f2af785a19424b99f61e75d6c66f90f4e8d2a67
Shauli Ravfogel <shauli321@gmail.com>
2020-01-14 08:52:42 +0200

    sklearn classifier class

--- src/classifier.py
+++ src/classifier.py
@@ -30,3 +30,33 @@ class Classifier(object):
 
 
 
+class SKlearnClassifier(Classifier):
+
+    def __init__(self, m):
+
+        self.model = m
+
+    def train_network(self, X_train: np.ndarray, Y_train: np.ndarray, X_dev: np.ndarray, Y_dev: np.ndarray) -> float:
+
+        """
+        :param X_train:
+        :param Y_train:
+        :param X_dev:
+        :param Y_dev:
+        :return: accuracy score on the dev set / Person's R in the case of regression
+        """
+
+        self.model.fit(X_train, Y_train)
+        score = self.model.score(X_dev, Y_dev)
+        return score
+
+    def get_weights(self) -> np.ndarray:
+        """
+        :return: final weights of the model, as np array
+        """
+
+        w = self.model.coef_
+        if len(w.shape) == 1:
+                w = np.expand_dims(w, 0)
+
+        return w
