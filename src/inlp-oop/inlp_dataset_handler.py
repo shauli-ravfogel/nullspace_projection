66acba282a7f161233785387fd48ca267db2d023
shauli <shauli321@gmail.com>
2020-02-09 14:36:36 +0200

    siamese dataset\

--- src/inlp-oop/inlp_dataset_handler.py
+++ src/inlp-oop/inlp_dataset_handler.py
@@ -31,6 +31,24 @@ class DatasetHandler(object):
         raise NotImplementedError
 
     def get_relevant_idx(self) -> Tuple[np.ndarray]:
+
+        if self.by_class:
+            if self.equal_chance_for_main_task_labels:
+                main_task_labels = list(set(self.Y_train_main.tolist()))
+                cls = random.choice(main_task_labels)
+            else:
+                cls = np.random.choice(self.Y_train_main)
+            relevant_idx_train = self.Y_train_main == cls
+            relevant_idx_dev = self.Y_dev_main == cls
+
+        else:
+
+            relevant_idx_train = np.ones(self.Y_train.shape[0], dtype=bool)
+            relevant_idx_dev = np.ones(self.Y_dev.shape[0], dtype=bool)
+
+        return relevant_idx_train, relevant_idx_dev
+
+    def get_relevant_idx(self) -> Tuple[np.ndarray]:
         """
         this function filter only the relevant indices from the training & dev set.
         if by_class=False, all idx are relevant; othewise, randomly choose the indices corrsponding to a single
@@ -60,23 +78,6 @@ class ClassificationDatasetHandler(DatasetHandler):
         self.X_train_current = P.dot(self.X_train_original.T).T
         self.X_dev_current = P.dot(self.X_dev_original.T).T
 
-    def get_relevant_idx(self) -> Tuple[np.ndarray]:
-
-        if self.by_class:
-            if self.equal_chance_for_main_task_labels:
-                main_task_labels = list(set(self.Y_train_main.tolist()))
-                cls = random.choice(main_task_labels)
-            else:
-                cls = np.random.choice(self.Y_train_main)
-            relevant_idx_train = self.Y_train_main == cls
-            relevant_idx_dev = Y_dev_main == cls
-
-        else:
-
-            relevant_idx_train = np.ones(X_train_cp.shape[0], dtype=bool)
-            relevant_idx_dev = np.ones(X_dev_cp.shape[0], dtype=bool)
-
-        return relevant_idx_train, relevant_idx_dev
 
     def get_current_training_set(self) -> Tuple[np.ndarray]:
 
@@ -86,10 +87,54 @@ class ClassificationDatasetHandler(DatasetHandler):
 
         relevant_idx_train , relevant_idx_dev = self.get_relevant_idx()
 
-        return self.X_train_current * dropout_mask[relevant_idx_train], self.Y_train[relevant_idx_train]
+        return (self.X_train_current * dropout_mask)[relevant_idx_train], self.Y_train[relevant_idx_train]
 
     def get_current_dev_set(self) -> Tuple[np.ndarray]:
 
         relevant_idx_train, relevant_idx_dev = self.get_relevant_idx()
         return self.X_dev_current[relevant_idx_dev], self.Y_dev[relevant_idx_dev]
 
+
+class SiameseDatasetHandle(DatasetHandler):
+
+    def __init__(self, X_train: Tuple[np.ndarray], Y_train: np.ndarray, X_dev: Tuple[np.ndarray], Y_dev: np.ndarray,
+                 dropout_rate = 0, Y_train_main = None, Y_dev_main = None, by_class = False, equal_chance_for_main_task_labels = True):
+
+            super().__init__(X_train, Y_train, X_dev, Y_dev,
+                 dropout_rate, Y_train_main, Y_dev_main, by_class, equal_chance_for_main_task_labels)
+
+    def apply_projection(self, P):
+
+        x_train1, x_train2 = self.X_train_original
+        x_train1_proj = P.dot(x_train1.T).T
+        x_train2_proj = P.dot(x_train2.T).T
+
+        x_dev, x_dev2 = self.X_dev_original
+        x_dev1_proj = P.dot(x_dev1.T).T
+        x_dev2_proj = P.dot(x_dev2.T).T
+
+        self.X_train_current = (x_train1_proj, x_train2_proj)
+        self.X_dev_current = (x_dev1_proj, x_dev2_proj)
+
+
+    def get_current_training_set(self) -> Tuple[np.ndarray]:
+
+        if self.dropout_rate > 0:
+            dropout_scale = 1. / (1 - self.dropout_rate + 1e-6)
+            dropout_mask = (np.random.rand(*self.X_train_current[0].shape) < (1 - dropout_rate)).astype(float) * dropout_scale
+
+        relevant_idx_train , relevant_idx_dev = self.get_relevant_idx()
+
+        X_train1, X_train2 = self.X_train_current
+        x_train_current_dropout_relevant = ((X_train1 * dropout_mask)[relevant_idx_train],
+                                            (X_train2 * dropout_mask[relevant_idx_train]))
+        return x_train_current_dropout_relevant, self.Y_train[relevant_idx_train]
+
+    def get_current_dev_set(self) -> Tuple[np.ndarray]:
+
+        relevant_idx_train, relevant_idx_dev = self.get_relevant_idx()
+        X_dev1, X_dev2 = self.X_dev_current
+        x_dev_current_relevant = ((X_dev1 * dropout_mask)[relevant_idx_dev],
+                                            (X_dev2 * dropout_mask[relevant_idx_dev]))
+
+        return x_dev_current_relevant, self.Y_dev[relevant_idx_dev]
--- src/inlp-oop/inlp_linear_model.py
+++ src/inlp-oop/inlp_linear_model.py
@@ -1,5 +1,9 @@
 import numpy as np
 import inlp_dataset_handler
+import torch
+from torch import nn
+import pytorch_lightning as pl
+from pytorch_lightning import Trainer
 
 # an abstract class for linear classifiers
 
@@ -64,3 +68,100 @@ class SKlearnClassifier(LinearModel):
                 w = np.expand_dims(w, 0)
 
         return w
+
+
+class Siamese(pl.LightningModule):
+
+    def __init__(self, X_train, X_dev, dim, batch_size, dropout_rate, device="cuda"):
+        super(Siamese, self).__init__()
+        d = 32
+        self.l1 = torch.nn.Linear(dim, 32, bias=True)
+        self.l2 = torch.nn.Linear(dim, 32, bias=True)
+        self.cosine_sim = torch.nn.CosineSimilarity(dim=1)
+        self.w1, self.w2, self.w3, self.b = torch.nn.Parameter(torch.rand(1)), torch.nn.Parameter(
+            torch.rand(1)), torch.nn.Parameter(torch.rand(1)), torch.nn.Parameter(torch.zeros(1))
+
+        self.train_data = Dataset(X_train, device)
+        self.dev_data = Dataset(X_dev, device)
+        self.train_gen = torch.utils.data.DataLoader(self.train_data, batch_size=batch_size, drop_last=False,
+                                                     shuffle=True)
+        self.dev_gen = torch.utils.data.DataLoader(self.dev_data, batch_size=batch_size, drop_last=False, shuffle=True)
+        self.dropout = torch.nn.Dropout(p=dropout_rate)
+        self.acc = None
+        # self.optimizer = torch.optim.Adam(self.parameters(), weight_decay = 1e-6)
+        self.loss_fn = torch.nn.BCEWithLogitsLoss()
+
+        self.net = torch.nn.Sequential(torch.nn.Linear(dim, 128), torch.nn.ReLU(), torch.nn.Linear(128, 16))
+
+    def forward(self, x1, x2):
+        h1 = self.l1(x1)
+        h2 = self.l2(x2)
+
+        # h1, h2 = self.net(x1), self.net(x2)
+
+        return h1, h2
+
+    def train_network(self, num_epochs):
+        trainer = Trainer(max_nb_epochs=num_epochs, min_nb_epochs=num_epochs, show_progress_bar=False)
+        trainer.fit(self)
+
+        return self.acc
+
+    def get_weights(self):
+        return self.l.weight.data.detach().cpu().numpy()
+
+    def get_final_representaton_for_sigmoid(self, h1, h2):
+        # norm1,norm2 =  torch.norm(h1, dim = 1, keepdim = True),  torch.norm(h2, dim = 1, keepdim = True)
+        # dot_prod = torch.sum(h1 * h2, axis = 1)
+        dot_prod = self.cosine_sim(h1, h2)
+        # dot_prod = torch.sum((h1-h2)**2, axis = 1)
+        # dot_prod = torch.sum(h1 - h2, axis = 1)
+        dot_prod = self.w3 * dot_prod + self.b
+        return dot_prod
+
+    def training_step(self, batch, batch_nb):
+        # REQUIRED
+        x1, x2, y = batch
+        h1, h2 = self.forward(self.dropout(x1), self.dropout(x2))
+        dot_prod = self.get_final_representaton_for_sigmoid(h1, h2)
+
+        loss_val = self.loss_fn(dot_prod, y)
+        correct = ((dot_prod > 0).int() == y.int()).int()
+        acc = torch.sum(correct).float() / len(y)
+
+        return {'loss': loss_val, 'val_acc': acc}
+
+    def validation_step(self, batch, batch_nb):
+        # OPTIONAL
+        x1, x2, y = batch
+        h1, h2 = self.forward(x1, x2)
+        dot_prod = self.get_final_representaton_for_sigmoid(h1, h2)
+
+        loss_val = self.loss_fn(dot_prod, y)
+        correct = ((dot_prod > 0).int() == y.int()).int()
+        acc = torch.sum(correct).float() / len(y)
+
+        return {'val_loss': loss_val, 'val_acc': acc}
+
+    def validation_end(self, outputs):
+        # OPTIONAL
+        avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
+        avg_acc = torch.stack([x['val_acc'] for x in outputs]).mean()
+        print("Loss is {}".format(avg_loss))
+        print("Accuracy is {}".format(avg_acc))
+        return {'avg_val_loss': avg_loss}
+
+    def configure_optimizers(self):
+        # REQUIRED
+        # return torch.optim.SGD(self.parameters(), lr=0.005, momentum=0.9)
+        return torch.optim.Adam(self.parameters(), weight_decay=1e-4)
+
+    @pl.data_loader
+    def train_dataloader(self):
+        return self.train_gen
+
+    @pl.data_loader
+    def val_dataloader(self):
+        # OPTIONAL
+        # can also return a list of val dataloaders
+        return self.dev_gen
