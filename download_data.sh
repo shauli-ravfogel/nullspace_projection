e1845df1baf64e6b2ebc9a9d06c6c01c8c8e829e
Shauli Ravfogel <shauli321@gmail.com>
2020-04-10 15:22:27 +0300

    download embeddings.

--- download_data.sh
+++ download_data.sh
@@ -3,12 +3,20 @@ mkdir data
 # We took the biasbios data from: https://github.com/IBM/sensitive-subspace-robustness
 # and performed the splits into train/dev/test to 65/10/25 percent accordingly with a random split per profession.
 # This is the same proportions from the original paper: https://arxiv.org/abs/1901.09451
-mkdir data/biasbios
+
+mkdir -p data/embeddings
+
+wget https://dl.fbaipublicfiles.com/fasttext/vectors-english/crawl-300d-2M.vec.zip -P data/embeddings/
+wget http://nlp.stanford.edu/data/glove.42B.300d.zip -P data/embeddings/
+unzip data/embeddings/crawl-300d-2M.vec.zip -d data/embeddings/crawl-300d-2M.vec
+unzip data/embeddings/glove.42B.300d.zip -d data/embeddings/glove.42B.300d.txt
+
+mkdir -p data/biasbios
 wget https://storage.googleapis.com/ai2i/nullspace/biasbios/train.pickle -P data/biasbios/
 wget https://storage.googleapis.com/ai2i/nullspace/biasbios/dev.pickle -P data/biasbios/
 wget https://storage.googleapis.com/ai2i/nullspace/biasbios/test.pickle -P data/biasbios/
 
-mkdir data/deepmoji
+mkdir -p data/deepmoji
 wget https://storage.googleapis.com/ai2i/nullspace/deepmoji/pos_pos.npy -P data/deepmoji
 wget https://storage.googleapis.com/ai2i/nullspace/deepmoji/pos_neg.npy -P data/deepmoji
 wget https://storage.googleapis.com/ai2i/nullspace/deepmoji/neg_pos.npy -P data/deepmoji
